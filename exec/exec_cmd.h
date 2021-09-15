7fe8e41b99b766a56a21b87b3bc77a939995aea5
nicholemaldonado4 <43501948+nicholemaldonado4@users.noreply.github.com>
2021-09-14 22:36:16 -0600

    Delete exec directory

--- exec/exec_cmd.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXEC_CMD_H
-#define EXEC_CMD_H
-
-#include "llist.h"
-
-bool execute(LList *cmds);
-
-#endif
--- exec/exec_from_file.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXECUTE_FROM_FILE_H
-#define EXECUTE_FROM_FILE_H
-
-#include "sub_cmd.h"
-
-LList *exec_from_file(SubCmd *sub_cmd);
-
-#endif
